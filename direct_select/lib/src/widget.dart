fb24252539f1d00418a3ccf4890a0cb9e2338142
Roland Volskaya <roland@volskaya.dev>
2020-09-29 11:11:33 +0300

    Fork [DirectSelect]
    + Allow setting [HitTestBehavior]

---
+++ direct_select/lib/src/widget.dart
@@ -0,0 +1,81 @@
+part of direct_select_plugin;
+
+class DirectSelect extends StatelessWidget {
+  /// Widget child you'll tap to display the Selection List.
+  final Widget child;
+
+  /// List of Widgets you'll display after you tap the child.
+  final List<Widget> items;
+
+  /// Listener when you select any item from the Selection List.
+  final ValueChanged<int> onSelectedItemChanged;
+
+  /// Height of each Item of the Selection List.
+  final double itemExtent;
+
+  /// Amount of magnification when showing the selected item in the overlay.
+  final double itemMagnification;
+
+  /// Selected index of your selection list.
+  final int selectedIndex;
+
+  /// The preferred method to engage this widget.
+  final DirectSelectMode mode;
+
+  /// Color of the background, [Colors.white] by default.
+  final Color backgroundColor;
+
+  /// [HitTestBehavior] of the gesture detector that's gonna wrap the child.
+  final HitTestBehavior hitTestBehavior;
+
+  const DirectSelect({
+    @required this.items,
+    @required this.onSelectedItemChanged,
+    @required this.itemExtent,
+    @required this.child,
+    this.selectedIndex = 0,
+    this.mode = DirectSelectMode.drag,
+    this.itemMagnification = 1.15,
+    this.backgroundColor = Colors.white,
+    this.hitTestBehavior = HitTestBehavior.opaque,
+    Key key,
+  })  : assert(items != null && items.length > 0),
+        assert(onSelectedItemChanged != null),
+        assert(itemExtent != null),
+        assert(child != null),
+        assert(selectedIndex != null && selectedIndex >= 0 && selectedIndex < items.length),
+        assert(mode != null),
+        assert(itemMagnification != null && itemMagnification >= 1.0),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    switch (mode) {
+      case DirectSelectMode.drag:
+        return _DirectSelectDrag(
+          selectedIndex: selectedIndex,
+          mode: mode,
+          itemMagnification: itemMagnification,
+          items: items,
+          onSelectedItemChanged: onSelectedItemChanged,
+          itemExtent: itemExtent,
+          backgroundColor: backgroundColor,
+          child: child,
+          hitTestBehavior: hitTestBehavior,
+        );
+      case DirectSelectMode.tap:
+        return _DirectSelectTap(
+          selectedIndex: selectedIndex,
+          mode: mode,
+          itemMagnification: itemMagnification,
+          items: items,
+          onSelectedItemChanged: onSelectedItemChanged,
+          itemExtent: itemExtent,
+          backgroundColor: backgroundColor,
+          child: child,
+          hitTestBehavior: hitTestBehavior,
+        );
+    }
+    throw UnimplementedError('Unknown DirectSelectMode provided: $mode');
+  }
+}
