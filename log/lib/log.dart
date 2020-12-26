b024ab7dbc78901d2753dbe63ff35fe804c25cab
Roland Volskaya <roland@volskaya.dev>
2020-12-26 11:24:51 +0200

    Expose logger's [Level]

--- log/lib/log.dart
+++ log/lib/log.dart
@@ -1,6 +1,7 @@
 library log;
 
 import 'package:logger/logger.dart';
+export 'package:logger/logger.dart' show Level;
 
 /// Easy logger constructor.
 class Log extends LogPrinter {
@@ -11,6 +12,16 @@ class Log extends LogPrinter {
 
   /// Creates a labeled [Log].
   static Logger named(String label) => Logger(printer: Log._(label));
+
+  /// The current logging level of the app.
+  ///
+  /// All logs with levels below this level will be omitted.
+  static Level get level => Logger.level;
+
+  /// The current logging level of the app.
+  ///
+  /// All logs with levels below this level will be omitted.
+  static set level(Level level) => Logger.level = level;
 
   @override
   List<String> log(LogEvent event) {
