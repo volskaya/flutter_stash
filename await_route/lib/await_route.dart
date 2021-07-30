9d562eec36745ebe7ec43eeebff8b8c0534bbcb4
Roland Volskaya <roland@volskaya.dev>
2021-07-30 18:16:31 +0300

    Return the [ModalRoute] from [AwaitRoute]

--- await_route/lib/await_route.dart
+++ await_route/lib/await_route.dart
@@ -2,9 +2,9 @@ library await_route;
 
 import 'dart:async';
 
-import 'package:flutter/scheduler.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 /// [AwaitRoute] awaits the end of route enter transition animation,
 /// of the [ModalRoute] tied to a [BuildContext].
@@ -12,22 +12,22 @@ import 'package:flutter/material.dart';
 /// Useful for views with input fields, where the auto focus should only fire
 /// after the route has finished its animation.
 abstract class AwaitRoute {
-  static Future<void> _awaitNow(BuildContext context) {
-    final route = ModalRoute.of(context);
-    if (route?.animation?.isCompleted ?? true) return SynchronousFuture<void>(null);
+  static Future<ModalRoute<T>?> _awaitNow<T extends Object?>(BuildContext context) {
+    final route = ModalRoute.of<T>(context);
+    if (route?.animation?.isCompleted ?? true) return SynchronousFuture<ModalRoute<T>?>(route);
 
-    final completer = Completer<void>();
+    final completer = Completer<ModalRoute<T>>();
     Timer? timeout;
     timeout = Timer(const Duration(seconds: 1) * timeDilation, () {
       if (timeout?.isActive == true) timeout?.cancel();
-      completer.complete();
+      completer.complete(route);
     });
 
     ValueChanged<AnimationStatus>? listener;
     listener = (AnimationStatus status) {
       if (status == AnimationStatus.completed && !completer.isCompleted) {
         if (timeout?.isActive == true) timeout?.cancel();
-        completer.complete();
+        completer.complete(route);
         if (listener != null) route!.animation!.removeStatusListener(listener);
       }
     };
@@ -36,28 +36,29 @@ abstract class AwaitRoute {
     return completer.future;
   }
 
-  static Future<void> _awaitInPostFrame(BuildContext context) {
-    if (WidgetsBinding.instance == null) return SynchronousFuture<void>(null);
+  static Future<ModalRoute<T>?> _awaitInPostFrame<T extends Object?>(BuildContext context) {
+    if (WidgetsBinding.instance == null) return SynchronousFuture<ModalRoute<T>?>(null);
 
-    final completer = Completer<void>();
-    WidgetsBinding.instance!.addPostFrameCallback((_) => _awaitNow(context).then(completer.complete));
+    final completer = Completer<ModalRoute<T>?>();
+    WidgetsBinding.instance!.addPostFrameCallback((_) => _awaitNow<T>(context).then(completer.complete));
     return completer.future;
   }
 
   /// Awaits the end of the [ModalRoute] enter animation.
   ///
   /// Use from [StatefulWidget.didChangeDependencies].
-  static Future<void> of(
+  static Future<ModalRoute<T>?> of<T extends Object?>(
     BuildContext context, {
 
     /// Await post frame, to allow this to be called from [StatefulWidget.initState].
     bool postFrame = false,
   }) =>
-      postFrame ? _awaitInPostFrame(context) : _awaitNow(context);
+      postFrame ? _awaitInPostFrame<T>(context) : _awaitNow<T>(context);
 }
 
 /// Extension of [BuildContext] to easily access [AwaitRoute] as a helper method.
 extension AwaitContextRoute on BuildContext {
   /// Awaits the end of the [ModalRoute] enter animation
-  Future awaitRoute({bool postFrame = false}) => AwaitRoute.of(this, postFrame: postFrame);
+  Future<ModalRoute<T>?> awaitRoute<T extends Object?>({bool postFrame = false}) =>
+      AwaitRoute.of<T>(this, postFrame: postFrame);
 }
